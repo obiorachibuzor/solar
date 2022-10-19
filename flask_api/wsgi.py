dc6cadb7d883add83b4f604f3952e64bb41755df
Chibuzor Obiora <218098562@student.uj.ac.za>
2022-10-19 09:58:16 +0200

    make it better

---
+++ flask_api/wsgi.py
@@ -0,0 +1,20 @@
+from app.main1 import app
+
+if __name__ == "__main1__":
+    app.run()
+
+from app.main2 import app
+
+if __name__ == "__main2__":
+    app.run()
+
+
+from app.main3 import app
+
+if __name__ == "__main3__":
+    app.run()
+
+from app.main4 import app
+
+if __name__ == "__main4__":
+    app.run()
